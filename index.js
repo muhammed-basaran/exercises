f87ecc009f4a308ac06a027e62c46f1b65e27a6e
Muhammed Basaran <mbasaran0293@gmail.com>
2022-11-09 16:27:22 +0100

    fix js problems

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function toggleButton() {
   const cards = document.querySelectorAll('[data-js=card]');
-  const buttons = card.querySelectorAll('[data-js=buttons]');
+  //const buttons = cards.querySelectorAll('[data-js=buttons]');
   cards.forEach(card => {
     const button = card.querySelector('[data-js="button"]');
-    const link = cards.querySelector('[data-js="link"]');
+    const link = card.querySelector('[data-js="link"]');
     const arrow = card.querySelector('[data-js="arrow"]');
     button.addEventListener('click', () => {
       link.classList.toggle('hide');
@@ -11,3 +11,5 @@ function toggleButton() {
     });
   });
 }
+
+toggleButton();
